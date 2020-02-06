9d1e2ce3f5280d18a1247a90c1ed894beafcb310
James Chapman <chapmanjamesg@gmail.com>
2020-02-06 12:58:28 -0600

    first commit

---
+++ libraryenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\chapm\workspace\backend\python\exercises\library-management\libraryproject\libraryenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
